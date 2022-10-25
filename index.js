5ee147ba2be66c78b638a759810cf4872a72a218
uziriahimu <uziriahimu@gmail.com>
2022-10-25 22:50:17 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 5000;
+
+const link = require('./data/link.json');
+
+app.get('/', (req, res) => {
+    res.send('API Running')
+})
+app.get('/link', (req, res) => {
+    res.send(link)
+});
+app.get('/link/:id', (req, res) => {
+    const id = req.params.id;
+    const links = news.filter(n => n.link_id === id);
+    res.send(links);
+
+})
+
+app.listen(port, () => {
+    console.log('education server running ', { port })
+})
