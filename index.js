0779688d5d2b5f5f227a7f241f46197f53d8e690
uziriahimu <uziriahimu@gmail.com>
2022-10-27 11:03:23 +0600

    sidebar data

--- index.js
+++ index.js
@@ -4,29 +4,31 @@ const cors = require('cors')
 const port = process.env.PORT || 5000;
 
 app.use(cors());
-const link = require('./data/link.json');
 const courses = require('./data/course.json');
+const categories = require('./data/category.json');
 
 app.get('/', (req, res) => {
     res.send('API Running')
 })
-app.get('/link', (req, res) => {
-    res.send(link)
-});
-app.get('/link/:id', (req, res) => {
-    const id = req.params.id;
-    const links = courses.filter(n => n.link_id === id);
-    res.send(links);
 
-})
 app.get('/courses', (req, res) => {
     res.send(courses);
 });
 app.get('/courses/:id', (req, res) => {
     const id = req.params.id;
-    const selectedCourses = courses.find(n => n._id === id);
+    const selectedCourses = courses.find(n => n.id === id);
     res.send(selectedCourses);
 });
+
+app.get('/categories', (req, res) => {
+    res.send(categories)
+});
+app.get('/category/:id', (req, res) => {
+    const id = req.params.id;
+    const category_data = courses.filter(n => n.category_id === id);
+    res.send(category_data);
+
+})
 
 app.listen(port, () => {
     console.log('education server running ', { port })
